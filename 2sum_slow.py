c1193937e919c3be612a48a01ca29ab243657ef1
okolonasty-msu <48096705+okolonasty-msu@users.noreply.github.com>
2022-10-31 16:02:28 -0400

    com

--- 2sum_slow.py
+++
@@ -1,9 +0,0 @@
-#O(n^2)
-# too slow solution
-def twoSum(nums, target):
-    for i in range(len(nums)):
-        for j in range(len(nums)):
-            if i != j:
-                if nums[i] + nums[j] == target:
-                    return list((i, j))
-                    break
